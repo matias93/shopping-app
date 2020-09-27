74ba2d83b18173849b72b9e3988b69a271484d9f
Matías González <maty.gr93@gmail.com>
2020-09-27 18:43:44 -0300

    creacion de data y componente de productos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React from 'react'
+import data from './data.json';
+import Products from './components/Products/Products';
 import './App.css';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-         <a href="/">React Shopping App</a>
-      </header>
-      <main>
-         Product List 
-      </main>
-      <footer>
-         All Rigth is reset. 
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products:data.products,
+      size:"",
+      sort:"",
+    };
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+           <a href="/">React Shopping App</a>
+        </header>
+        <main>
+          <div className="contenido">
+            <div className="main">
+               <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">
+              Card Items
+            </div>
+          </div>
+        </main>
+        <footer>
+           All Rigth is reset. 
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Products.css';
+
+export default class Products extends Component {
+    render() {
+        
+        return (
+           <div>
+             <ul className="productos">
+                 {this.props.products.map((product) => (
+                  <li key={product.id}>
+                      <div className="producto">
+                         <a href={"#" + product.id}>
+                              <img src={product.image} alt="bicicleta"/>
+                              <p>{product.title}</p>
+                          </a>
+                          <div className="precio-precio">
+                              <div>
+                                  {product.price}
+                              </div>
+                              <button className="button primary">
+                                  add to card
+                              </button>
+                          </div>
+                      </div>
+                  </li>
+
+                 ))}
+             </ul>
+           </div>
+        )
+    }
+}
